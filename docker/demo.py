f6b1dff086146a70205275afbd81c84e2c387f86
Azzatul Nabila <azzatulnabila579@gmail.com>
2025-10-28 08:25:09 +0800

    up

---
+++ docker/demo.py
@@ -0,0 +1,37 @@
+import requests
+import time
+
+# Lock Manager
+print("Node1 acquire lock")
+r1 = requests.post('http://node1:5000/lock/acquire', json={'resource':'r1','mode':'exclusive'})
+print(r1.text)
+
+print("Node2 try acquire lock (waiting)")
+r2 = requests.post('http://node2:5000/lock/acquire', json={'resource':'r1','mode':'exclusive'})
+print(r2.text)
+
+print("Node1 release lock")
+r3 = requests.post('http://node1:5000/lock/release', json={'resource':'r1'})
+print(r3.text)
+
+print("Node2 acquire lock again")
+r4 = requests.post('http://node2:5000/lock/acquire', json={'resource':'r1','mode':'exclusive'})
+print(r4.text)
+
+# Queue
+print("Node2 produce message")
+rq = requests.post('http://node2:5000/queue/produce', json={'topic':'t','message':'hello'})
+print(rq.text)
+
+print("Node3 consume message")
+rc = requests.post('http://node3:5000/queue/consume', json={'topic':'t'})
+print(rc.text)
+
+# Cache
+print("Node1 write cache")
+rw = requests.post('http://node1:5000/cache/write', json={'key':'k1','value':'v1'})
+print(rw.text)
+
+print("Node3 read cache")
+rread = requests.get('http://node3:5000/cache/read', params={'key':'k1'})
+print(rread.text)
